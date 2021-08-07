4594ff374f6ea104af8efd01dbd35345a35ca969
vivly <qmjxt@outlook.com>
2021-08-07 16:13:09 +0800

    first commit

--- downloader.py
+++ downloader.py
@@ -12,7 +12,7 @@ import re
 import requests
 
 
-def download(downloadDF):
+def download(downloadDF, path):
     n = len(downloadDF)
     linkFormula = re.compile('(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]')
     for i in range(0, n - 1):
@@ -29,25 +29,27 @@ def download(downloadDF):
                 matchObject = linkFormula.search(tempList[k])
                 if matchObject is not None:
                     linkCount = linkCount + 1
-                    print(idString + "，正在下载图片...")
-                    download_img(matchObject.group(0),i)
-                    urlretrieve(matchObject.group(0), './image/img'+ str(i) + '.png')
-                    print(idString + "，图片下载完成，" + "共" + str(linkCount) + "张图片.")
+                    print(idString + "，编号:" + str(i) +"，正在下载图片...")
+                    download_img(matchObject.group(0), i, path, idString,linkCount)
+                    # urlretrieve(matchObject.group(0), './image/img'+ str(i) + '.png')
+                    print(idString + "，编号:" + str(i) + "，图片下载完成，" + "第" + str(linkCount) + "张图片.")
 
-def download_img(img_url,i):
+def download_img(img_url, i, path, id, k):
     print(img_url)
-    header = {'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/'
+    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/'
               '537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
     r = requests.get(img_url, headers=header, stream=True)
-    print(r.status_code) # 返回状态码
+    print("状态码：" + str(r.status_code)) # 返回状态码
     if r.status_code == 200:
-        open('./image/img'+ str(i) + '.png', 'wb').write(r.content) # 将内容写入图片
+        open(path + '\\' + id + '_' + str(i) + '_(' + str(k) + ').png', 'wb').write(r.content) # 将内容写入图片
+    else:
+        print("状态码有误！")
     del r
 
 
 
 def downloadlistfetcher(path):
-    csvData = pd.read_csv(path)
+    csvData = pd.read_csv(path, low_memory=False)
     dfData = pd.DataFrame(csvData)
     downloadDict = []
     downloadList = pd.DataFrame(downloadDict)
@@ -67,8 +69,10 @@ def checkdir(path):
 
 
 if __name__ == '__main__':
-    downloadPath = 'E:\WorkSpace\Python\Twint\data\italy\event1_pic'
-    readPath = 'E:\WorkSpace\Python\Twint\data\italy\event1\event1.csv'
+    downloadPath = 'D:\WorkSpace\Python\Twint\data\italy\event14_pic'
+    readPath = 'D:\WorkSpace\Python\Twint\data\italy\event14\event14.csv'
     checkdir(downloadPath)
     downloadDF = downloadlistfetcher(readPath)
-    download(downloadDF)
+    download(downloadDF, downloadPath)
+    print("所有图片下载完成。")
+
--- modification.py
+++ modification.py
@@ -3,7 +3,7 @@ import re
 
 
 def readcsv(path):
-    csvData = pd.read_csv(path)
+    csvData = pd.read_csv(path, low_memory=False)
     dfData = pd.DataFrame(csvData)
     print("Original data length is " + str(len(dfData) - 1))
     print("Column name is " + str([column for column in dfData]))
@@ -27,15 +27,19 @@ def modify(list, dfdata):
     n = len(data_after_duplicated)
     date_formula = re.compile(
         '(([1-3][0-9]{3})[-]{0,1}(((0[13578]|1[02])[-]{0,1}(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)[-]{0,1}'
-        '(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8])))\s\d{1,2}:\d{1,2}:\d{1,2})|(([1-3][0-9]{3})[-]'
+        '(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-9])))\s\d{1,2}:\d{1,2}:\d{1,2})|(([1-3][0-9]{3})[-]'
         '{0,1}(((0[13578]|1[02])[-]{0,1}(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-'
         '(0[1-9]|[1][0-9]|2[0-8])))\s\d{1,2}:\d{1,2})|(([1-3][0-9]{3})[-]{0,1}(((0[13578]|1[02])[-]{0,1}'
         '(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)[-]{0,1}(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))')
     tempDf = pd.DataFrame(data_after_duplicated)
     for i in range(0, n):
         if if_contain_symbol(data_after_duplicated.iloc[i, 2]):
-            tempDf.iloc[i, 2] = (date_formula.search(data_after_duplicated.iloc[i, 2]).group(0) + " 中国标准时间")
-            print(tempDf.iloc[i, 2])
+            try:
+                tempDf.iloc[i, 2] = (date_formula.search(data_after_duplicated.iloc[i, 2]).group(0) + " 中国标准时间")
+                print(tempDf.iloc[i, 2])
+            except AttributeError:
+                print("该字符为非法字符")
+                continue
     print('Modify complete')
     return tempDf
 
@@ -44,17 +48,30 @@ def savetoPath(path, dfdata):
     dfdata.to_csv(path, index=None)
     print('Save complete')
 
+def testFormula():
+    date_formula = re.compile(
+        '(([1-3][0-9]{3})[-]{0,1}(((0[13578]|1[02])[-]{0,1}(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)[-]{0,1}'
+        '(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-9])))\s\d{1,2}:\d{1,2}:\d{1,2})|(([1-3][0-9]{3})[-]'
+        '{0,1}(((0[13578]|1[02])[-]{0,1}(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-'
+        '(0[1-9]|[1][0-9]|2[0-8])))\s\d{1,2}:\d{1,2})|(([1-3][0-9]{3})[-]{0,1}(((0[13578]|1[02])[-]{0,1}'
+        '(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)[-]{0,1}(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))')
+    print(date_formula.search("2020-02-29 07:57:11"))
+
+
 
 def if_contain_symbol(keyword):
-    if re.search(r"\W", keyword):
-        return True
-    else:
+    try:
+        if re.search(r"\W", keyword):
+            return True
+        else:
+            return False
+    except TypeError:
         return False
 
 
 if __name__ == '__main__':
-    readPath = 'E:\WorkSpace\Python\Twint\data\italy\event1\original-event1.csv'
-    writePath = 'E:\WorkSpace\Python\Twint\data\italy\event1\event1.csv'
+    readPath = 'D:\WorkSpace\Python\Twint\data\italy\event14\original-event14.csv'
+    writePath = 'D:\WorkSpace\Python\Twint\data\italy\event14\event14.csv'
     dfData = readcsv(readPath)
     duplicateList = detectcsv(dfData)
     dfData = modify(duplicateList, dfData)
